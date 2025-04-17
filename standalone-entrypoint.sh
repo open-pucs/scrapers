de1712f59beb87b685aabfdd49f2eeecac682090
Nicole Venner <nvenner@protonmail.ch>
2025-04-17 17:19:19 -0600

    hopefully fix some stuff

---
+++ standalone-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Start Airflow in the background
+airflow standalone &
+
+# Wait until Airflow's webserver (port 8080) is available
+while ! nc -z localhost 8080; do
+  sleep 1
+done
+
+# Start socat to forward port 8081 -> 8080
+exec socat TCP-LISTEN:8081,fork,reuseaddr TCP:localhost:8080
