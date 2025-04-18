6a3b7d6dcf9a3feec850eb4d39c38dbce19320fe
Nicole Venner <nvenner@protonmail.ch>
2025-04-17 18:47:05 -0600

    finally got airflow working

--- standalone-entrypoint.sh
+++ standalone-entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+
+echo "The script has started."
 # Start Airflow in the background
 airflow standalone &
 
 # Wait until Airflow's webserver (port 8080) is available
-while ! nc -z localhost 8080; do
-  sleep 1
-done
+# while ! nc -z localhost 8080; do
+#   sleep 1
+# done
 
 # Start socat to forward port 8081 -> 8080
 exec socat TCP-LISTEN:8081,fork,reuseaddr TCP:localhost:8080
