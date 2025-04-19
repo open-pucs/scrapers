c67baf46455aba1156614660613fbf8e3ebb90fc
Nicole Venner <nvenner@protonmail.ch>
2025-04-19 07:15:21 -0600

    modify scraper exports

--- openpuc_scrapers/scrapers/__init__.py
+++ openpuc_scrapers/scrapers/__init__.py
@@ -1,3 +1,4 @@
-from .ma import MassachusettsDPU
+# from openpuc_scrapers.scrapers.ma_puc import MassachusettsDPU
+from openpuc_scrapers.scrapers.ny_puc import NYPUCScraper
 
-__all__ = ["MassachusettsDPU"]
+__all__ = ["NYPUCScraper"]
