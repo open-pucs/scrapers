653b674656ae6085cf29ef479ab1e560b259650f
Nicole Venner <nvenner@protonmail.ch>
2025-04-19 06:54:59 -0600

    typo in dependancy

--- openpuc_scrapers/pipelines/helper_utils.py
+++ openpuc_scrapers/pipelines/helper_utils.py
@@ -3,7 +3,7 @@ from typing import Any
 from pydantic import BaseModel
 
 
-from openpuc_scrapers.pipelines.s3_utils import S3FileManager
+from openpuc_scrapers.db.s3_utils import S3FileManager
 
 
 # Helper functions
