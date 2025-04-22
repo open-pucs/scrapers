1ee666796efd6cca1fa037d9630472289eef59d1
Nicole Venner <nvenner@protonmail.ch>
2025-04-22 09:52:42 -0600

    add ilinois to scraper registry

--- openpuc_scrapers/scrapers/scraper_lookup.py
+++ openpuc_scrapers/scrapers/scraper_lookup.py
@@ -3,15 +3,8 @@ from pydantic import BaseModel
 
 from openpuc_scrapers.scrapers.base import GenericScraper
 from openpuc_scrapers.scrapers.dummy import DummyScraper
+from openpuc_scrapers.scrapers.il import IllinoisICCScraper
 from openpuc_scrapers.scrapers.ny_puc import NYPUCScraper
-
-
-# class ScraperName(str, Enum):
-#     NY_PUC = "ny_puc"
-#     MA_PUC = "ma_puc"
-#     IL_PUC = "il_puc"
-#     CO_PUC = "co_puc"
-#     DUMMY = "dummy"
 
 
 class ScraperInfoObject(BaseModel):
@@ -23,7 +16,7 @@ class ScraperInfoObject(BaseModel):
 SCRAPER_REGISTRY: List[ScraperInfoObject] = [
     ScraperInfoObject(id="ny_puc", name="New York PUC", object_type=NYPUCScraper),
     ScraperInfoObject(id="dummy", name="Dummy Scraper", object_type=DummyScraper),
+    ScraperInfoObject(id="il_puc", name="Illinois ICC", object_type=IllinoisICCScraper),
     # ScraperInfoObject(id="ma_puc", name="Massachusetts PUC", object_type=MAPUCScraper),
-    # ScraperInfoObject(id="il_puc", name="Illinois PUC", object_type=ILPUCScraper),
     # ScraperInfoObject(id="co_puc", name="Colorado PUC", object_type=COPUCScraper),
 ]
