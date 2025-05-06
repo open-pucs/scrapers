55e17ccf22f6f10017ebe1c7f4596cfda3b796e6
Nicole Venner <nvenner@protonmail.ch>
2025-05-05 19:51:35 -0600

    fix verbosity

--- openpuc_scrapers/db/sql_utilities.py
+++ openpuc_scrapers/db/sql_utilities.py
@@ -101,9 +101,7 @@ async def get_all_cases_from_jurisdiction(
     caseidlist = await s3.list_objects_with_prefix_async(prefix=prefix)
 
     def gen_caseinfo(case_id: str) -> CaseInfoMinimal:
-        case_id_minified = case_id.split("/")[-1].split(".json")[
-            0
-        ]  # Extract filename without path or extension
+        case_id_minified = case_id.split("/")[-1].removesuffix(".json")
         return CaseInfoMinimal(
             case_id=case_id_minified,
             jurisdiction_name=jurisdiction_name,
