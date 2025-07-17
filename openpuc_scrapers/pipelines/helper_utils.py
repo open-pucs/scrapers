532d9ad19772bc92f070b9141a24e2b0205d8fe2
Nicole Venner <nvenner@protonmail.ch>
2025-07-17 12:20:15 -0600

    try another fix

--- openpuc_scrapers/pipelines/helper_utils.py
+++ openpuc_scrapers/pipelines/helper_utils.py
@@ -8,10 +8,10 @@ from openpuc_scrapers.models.constants import OPENSCRAPERS_INTERNAL_API_URL
 
 
 def save_json_sync(path: str, data: Any, bucket: Optional[str] = None) -> None:
-    if data.isinstance(BaseModel):
-        data = data.dict()
     url = f"{OPENSCRAPERS_INTERNAL_API_URL}/admin/write_openscrapers_s3_json"
     payload = {"key": path, "contents": data}
+    if isinstance(data, BaseModel):
+        payload["data"] = data.model_dump()
     if bucket is not None:
         payload["bucket"] = bucket
     response = requests.post(url, json=payload)
