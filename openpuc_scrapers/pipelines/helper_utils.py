ec835dce3fe790e706c2bccbbf1aa1420a4db119
Nicole Venner <nvenner@protonmail.ch>
2025-07-17 12:09:56 -0600

    hopeful fix for pydantic error codes?

--- openpuc_scrapers/pipelines/helper_utils.py
+++ openpuc_scrapers/pipelines/helper_utils.py
@@ -8,6 +8,8 @@ from openpuc_scrapers.models.constants import OPENSCRAPERS_INTERNAL_API_URL
 
 
 def save_json_sync(path: str, data: Any, bucket: Optional[str] = None) -> None:
+    if data.isinstance(BaseModel):
+        data = data.dict()
     url = f"{OPENSCRAPERS_INTERNAL_API_URL}/admin/write_openscrapers_s3_json"
     payload = {"key": path, "contents": data}
     if bucket is not None:
