b6c00eb8a9a58f7d8ac20e95e391794f8eb6e56d
Nicole Venner <nvenner@protonmail.ch>
2025-07-17 15:11:13 -0600

    try even moar shit

--- openpuc_scrapers/pipelines/helper_utils.py
+++ openpuc_scrapers/pipelines/helper_utils.py
@@ -11,12 +11,31 @@ from openpuc_scrapers.models.constants import OPENSCRAPERS_INTERNAL_API_URL
 default_logger = logging.getLogger(__name__)
 
 
+def create_json_string(data: Any) -> str:
+    def _serialize(obj):
+        if isinstance(obj, BaseModel):
+            return obj.model_dump(mode="json")
+            # return json.loads(obj.model_dump_json())
+        return obj
+
+    if isinstance(data, BaseModel):
+        return data.model_dump_json(indent=2)
+
+    if isinstance(data, list):
+        return json.dumps([_serialize(item) for item in data], indent=2)
+
+    if isinstance(data, dict):
+        return json.dumps({k: _serialize(v) for k, v in data.items()}, indent=2)
+
+    raise ValueError(f"Unsupported data type: {type(data)}")
+
+
 def save_json_sync(path: str, data: Any, bucket: Optional[str] = None) -> None:
     try:
         url = f"{OPENSCRAPERS_INTERNAL_API_URL}/admin/write_openscrapers_s3_json"
-        payload = {"key": path, "contents": data}
-        if isinstance(data, BaseModel):
-            payload["data"] = data.model_dump()
+        json_str = create_json_string(data)
+        json_obj = json.loads(json_str)
+        payload = {"key": path, "contents": json_obj}
         if bucket is not None:
             payload["bucket"] = bucket
         response = requests.post(url, json=payload)
