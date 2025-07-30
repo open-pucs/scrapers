fd7f281f5f9cf1436d79c512aee6862e8d670d94
Nicole Venner <nvenner@protonmail.ch>
2025-07-29 18:30:45 -0600

    new tables and stuff

--- js_scrapers/load-data.py
+++ js_scrapers/load-data.py
@@ -15,14 +15,17 @@ else:
     db = libsql_client.create_client_sync(url="file:wells.db")
 
 # Drop tables if they exist to ensure schema updates
-db.batch([
-    libsql_client.Statement("DROP TABLE IF EXISTS wells;"),
-    libsql_client.Statement("DROP TABLE IF EXISTS permits;")
-])
+db.batch(
+    [
+        libsql_client.Statement("DROP TABLE IF EXISTS wells;"),
+        libsql_client.Statement("DROP TABLE IF EXISTS permit_file_data;"),
+    ]
+)
 
 # Create tables
-db.batch([
-    libsql_client.Statement("""
+db.batch(
+    [
+        libsql_client.Statement("""
       CREATE TABLE IF NOT EXISTS wells (
         api_well_number TEXT PRIMARY KEY,
         operator TEXT,
@@ -56,8 +59,8 @@ db.batch([
         total_carbon_emissions REAL
       );
     """),
-    libsql_client.Statement("""
-      CREATE TABLE IF NOT EXISTS permits (
+        libsql_client.Statement("""
+      CREATE TABLE IF NOT EXISTS permit_file_data (
         permit_id INTEGER PRIMARY KEY AUTOINCREMENT,
         api_well_number TEXT,
         log_category TEXT,
@@ -68,12 +71,14 @@ db.batch([
         well_status TEXT,
         FOREIGN KEY (api_well_number) REFERENCES wells(api_well_number)
       );
-    """)
-])
+    """),
+    ]
+)
+
 
 # Load and insert wells data
 with open(
-    "/home/nicole/Documents/mycorrhiza/scrapers/js_scrapers/cypress/downloads/well_data_both_options.csv",
+    "/home/nicole/Documents/mycorrhiza/scrapers/js_scrapers/raw_data/downloads/well_data_both_options.csv",
     "r",
     encoding="utf-8",
 ) as wells_file:
@@ -84,8 +89,9 @@ with open(
         cum_gas = float(well.get("Cumulative\nNatural Gas\n(MCF)", 0) or 0)
         total_carbon_emissions = (cum_oil * 0.43) + (cum_gas * 0.055)
 
-        well_statements.append(libsql_client.Statement(
-            """
+        well_statements.append(
+            libsql_client.Statement(
+                """
             INSERT OR IGNORE INTO wells (
                 api_well_number, operator, well_name, well_status, well_type,
                 coalbed_methane_well, cumulative_oil_barrels, cumulative_natural_gas_mcf,
@@ -102,46 +108,46 @@ with open(
                 :elev_kb, :slant, :td, :pbtd, :confidential, :total_carbon_emissions
             )
             """,
-            {
-                "api_well_number": well.get("API Well Number"),
-                "operator": well.get("Operator"),
-                "well_name": well.get("Well Name"),
-                "well_status": well.get("Well Status"),
-                "well_type": well.get("Well Type"),
-                "coalbed_methane_well": well.get("Coalbed\nMethane\nWell?"),
-                "cumulative_oil_barrels": cum_oil,
-                "cumulative_natural_gas_mcf": cum_gas,
-                "cumulative_water_barrels": float(
-                    well.get("Cumulative\nWater\n(Barrels)", 0) or 0
-                ),
-                "field_name": well.get("Field Name"),
-                "surface_ownership": well.get("Surface\nOwnership"),
-                "mineral_lease": well.get("Mineral\nLease"),
-                "county": well.get("County"),
-                "qtr_qtr": well.get("Qtr/Qtr"),
-                "section": well.get("Section"),
-                "township_range": well.get("Township-Range"),
-                "fnl_fsl": well.get("FNL/FSL"),
-                "fel_fwl": well.get("FEL/FWL"),
-                "utm_eastings": float(well.get("UTM\nEastings", 0) or 0),
-                "utm_northings": float(well.get("UTM\nNorthings", 0) or 0),
-                "latitude": float(well.get("Latitude", 0) or 0),
-                "longitude": float(well.get("Longitude", 0) or 0),
-                "elev_gr": float(well.get("Elev .GR", 0) or 0),
-                "elev_df": float(well.get("Elev. DF", 0) or 0),
-                "elev_kb": float(well.get("Elev. KB", 0) or 0),
-                "slant": well.get("Slant"),
-                "td": float(well.get("TD", 0) or 0),
-                "pbtd": float(well.get("PBTD", 0) or 0),
-                "confidential": well.get("Confidential"),
-                "total_carbon_emissions": total_carbon_emissions,
-            },
-        ))
+                {
+                    "api_well_number": well.get("API Well Number"),
+                    "operator": well.get("Operator"),
+                    "well_name": well.get("Well Name"),
+                    "well_status": well.get("Well Status"),
+                    "well_type": well.get("Well Type"),
+                    "coalbed_methane_well": well.get("Coalbed\nMethane\nWell?"),
+                    "cumulative_oil_barrels": cum_oil,
+                    "cumulative_natural_gas_mcf": cum_gas,
+                    "cumulative_water_barrels": float(
+                        well.get("Cumulative\nWater\n(Barrels)", 0) or 0
+                    ),
+                    "field_name": well.get("Field Name"),
+                    "surface_ownership": well.get("Surface\nOwnership"),
+                    "mineral_lease": well.get("Mineral\nLease"),
+                    "county": well.get("County"),
+                    "qtr_qtr": well.get("Qtr/Qtr"),
+                    "section": well.get("Section"),
+                    "township_range": well.get("Township-Range"),
+                    "fnl_fsl": well.get("FNL/FSL"),
+                    "fel_fwl": well.get("FEL/FWL"),
+                    "utm_eastings": float(well.get("UTM\nEastings", 0) or 0),
+                    "utm_northings": float(well.get("UTM\nNorthings", 0) or 0),
+                    "latitude": float(well.get("Latitude", 0) or 0),
+                    "longitude": float(well.get("Longitude", 0) or 0),
+                    "elev_gr": float(well.get("Elev .GR", 0) or 0),
+                    "elev_df": float(well.get("Elev. DF", 0) or 0),
+                    "elev_kb": float(well.get("Elev. KB", 0) or 0),
+                    "slant": well.get("Slant"),
+                    "td": float(well.get("TD", 0) or 0),
+                    "pbtd": float(well.get("PBTD", 0) or 0),
+                    "confidential": well.get("Confidential"),
+                    "total_carbon_emissions": total_carbon_emissions,
+                },
+            )
+        )
     db.batch(well_statements)
 
 # Load and insert permits data
-old_permit_data_path = "/home/nicole/Documents/mycorrhiza/scrapers/js_scrapers/cypress/downloads/utah_dogm_file_data-round-1.csv"
-new_permit_data_path = "/home/nicole/Documents/mycorrhiza/scrapers/js_scrapers/cypress/downloads/get_all_permit_data_ad_hoc.csv"
+new_permit_data_path = "/home/nicole/Documents/mycorrhiza/scrapers/js_scrapers/cypress/downloads/get_all_permit_file_data.csv"
 
 with open(
     new_permit_data_path,
@@ -153,25 +159,31 @@ with open(
     next(permits_reader)  # Skip header row
     permit_statements = []
     for permit_row in permits_reader:
-        date_posted = datetime.strptime(permit_row[3], "%m/%d/%Y").date() if permit_row[3] else None
-        permit_statements.append(libsql_client.Statement(
-            """
-            INSERT INTO permits (
+        date_posted = (
+            datetime.strptime(permit_row[3], "%m/%d/%Y").date()
+            if permit_row[3]
+            else None
+        )
+        permit_statements.append(
+            libsql_client.Statement(
+                """
+            INSERT INTO permit_file_data (
                 api_well_number, log_category, log_type, date_posted, pdf_link, operator, well_status
             ) VALUES (
                 :api_well_number, :log_category, :log_type, :date_posted, :pdf_link, :operator, :well_status
             )
         """,
-            {
-                "api_well_number": permit_row[0],
-                "log_category": permit_row[1],
-                "log_type": permit_row[2],
-                "date_posted": date_posted.isoformat() if date_posted else None,
-                "pdf_link": permit_row[7],
-                "operator": permit_row[8],
-                "well_status": permit_row[11],
-            },
-        ))
+                {
+                    "api_well_number": permit_row[0],
+                    "log_category": permit_row[1],
+                    "log_type": permit_row[2],
+                    "date_posted": date_posted.isoformat() if date_posted else None,
+                    "pdf_link": permit_row[7],
+                    "operator": permit_row[8],
+                    "well_status": permit_row[11],
+                },
+            )
+        )
     db.batch(permit_statements)
 
 # Create the computed view
@@ -184,7 +196,7 @@ SELECT
   MIN(p.date_posted) AS earliest_permit_date,
   MAX(p.date_posted) AS latest_permit_date
 FROM wells w
-JOIN permits p ON w.api_well_number = p.api_well_number
+JOIN permit_file_data p ON w.api_well_number = p.api_well_number
 GROUP BY w.api_well_number;
 """)
 
---
+++ js_scrapers/new_table_instructions.md
@@ -0,0 +1,72 @@
+# New Tables:
+
+Could you create new tables with the following data:
+
+- Name: application_for_permit_drilling_granted
+- FileName:
+  /home/nicole/Documents/mycorrhiza/scrapers/js_scrapers/raw_data/permits_approved_all_raw_data.csv
+
+```csv
+
+"Date
+Posted to Web","Date
+Approved","APD
+Number","County","Operator","Well Name","API
+Number","Work
+Type","Well
+Type","Current
+Status","Field","Surface Location","Qtr Qtr","Section","Township","Range","UTM
+Northings","UTM
+Eastings","Latitude","Longitude","Elevation","Planned
+Depth (MD)","Proposed
+Zone","Directional/
+Horizontal","Confidential
+Well?"
+"07/22/2025","07/21/2025","16380","UINTAH","Anschutz Exploration Corporation","RAINIER FED 1124-23-35-14 MCH","4304757848","DRILL","Gas Well","Approved Permit","UNDESIGNATED","2179 FNL 2316 FWL","LOT4","23","11S","24E","4412526.0","654040.0","39.848793","-109.19942","6020","20781","MNCS ","HORIZONTAL","Y"
+"07/10/2025","07/09/2025","16309","UINTAH","Middle Fork Energy Uinta, LLC","ECHO 9A1-36-622","4304757810","DRILL","Gas Well","Approved Permit","UNDESIGNATED","345 FNL 760 FEL","LOT1","1","7S","22E","4456683.0","637646.0","40.24929","-109.38159","5139","12452","MVRD ","DIRECTIONAL","Y"
+"07/10/2025","07/09/2025","16308","UINTAH","Middle Fork Energy Uinta, LLC","ECHO 8D4-36-622","4304757811","DRILL","Gas Well","Approved Permit","UNDESIGNATED","330 FNL 760 FEL","LOT1","1","7S","22E","4456687.0","637646.0","40.24933","-109.38159","5138","12507","MVRD ","DIRECTIONAL","Y"
+```
+
+- Name: historical_well_metadata
+- FileName:
+  /home/nicole/Documents/mycorrhiza/scrapers/js_scrapers/raw_data/well_history_all.csv
+-
+
+```csv
+"API Well Number","Event <br /> Work Type","APD <br /> Approval","Spud Date <br /> Dry","Spud Date <br /> Rotary","Completion <br /> Date","Sundry of <br /> Intent","Sundry Work <br /> Complete","First <br /> Production","Historical Well Status","Well Type","Producing <br /> Zone","TD- <br /> MD","TD- <br /> TVD","PBTD- <br /> MD","PBTD- <br /> TVD","Production <br /> Test Method","Oil <br /> 24hr Test","Gas <br /> 24hr Test","Water <br /> 24hr Test","Choke","Tubing  <br /> Pressure","Casing  <br /> Pressure","Dir. <br /> Survey","Cored","DST","Completion <br /> Type","Directional","Horiz. <br /> Laterals","Confidential?"
+"4300130001","DRILL","12/13/1974","12/28/1974","","03/21/1975","","","","Plugged & Abandoned","","","8540.0","","0.0","0.0","","","","","","","","Y","N","Y","","Directional","0","N"
+"4300130003","DRILL","11/24/1980","","","","","","","Location Abandoned - APD rescinded","","","0.0","","0.0","0.0","","","","","","","","N","N","N","","","0","N"
+"4300130004","DRILL","11/25/1980","02/02/1981","","03/08/1981","","","","Plugged & Abandoned","","","7048.0","","0.0","0.0","","","","","","","","N","N","N","","Vertical","0","N"
+"4300130005","DRILL","01/09/1981","03/31/1981","04/19/1981","07/02/1981","","","","Plugged & Abandoned","","","11367.0","","0.0","0.0","","","","","","","","N","N","N","","Vertical","0","N"
+"4300130006","DRILL","02/24/1993","04/19/1993","","02/19/1994","","","","Plugged & Abandoned","","","12400.0","","0.0","0.0","","","","","","","","Y","N","N","","Vertical","0","N"
+"4300130007","DRILL","06/10/2008","08/21/2008","09/20/2008","11/22/2008","","","","Plugged & Abandoned","","","11854.0","","0.0","0.0","","","","","","","","N","N","N","","Vertical","0","N"
+```
+
+- Name: apd_permit_expiary
+- FileName:
+  /home/nicole/Documents/mycorrhiza/scrapers/js_scrapers/raw_data/all_permit_expiary_dates.csv
+
+```csv
+"Operator","API
+Number","Well Name","Work
+Type","Date
+Approved","Date
+Permit Will
+Expire"
+"A1 Lithium, Inc","4301950095","LCW-1","DRILL","10/15/2021","10/15/2024"
+"A1 Lithium, Inc","4301950096","LCW-2","DRILL","10/18/2021","10/15/2024"
+"AC Oil, LLC","4304757198","AC-4D","DRILL","02/21/2025","02/21/2026"
+"American Helium Operating, LLC","4303750087","Gallant Fox 1","DRILL","03/05/2020","03/05/2024"
+"American Helium Operating, LLC","4303750090","Big Indian Fed 24-31B-30-25","DRILL","05/19/2022","05/19/2024"
+"American Potash, LLC","4301950111","AP-S-2 Duma Point","DRILL","01/08/2024","01/07/2025"
+"American Potash, LLC","4301950112","Ten Mile AP-S-16","DRILL","01/08/2024","01/07/2025"
+"American Potash, LLC","4301950113","Mineral Springs AP-S-36 ","DRILL","01/08/2024","01/07/2025"
+"American Potash, LLC","4301950114","AP-F-34 Keg Springs","DRILL","01/08/2024","01/07/2025"
+"American Potash, LLC","4301950115","AP-F-24 Trail Canyon","DRILL","01/08/2024","01/07/2025"
+"American Potash, LLC","4301950116","AP-F-28 Lost World","DRILL","01/08/2024","01/07/2025"
+"American Potash, LLC","4301950117","AP-F-12 Hey Joe","DRILL","01/08/2024","01/07/2025"
+"Anschutz Exploration Corporation","4304757524","CENTER FORK FED 12-24-17-20-14MCH","DRILL","02/22/2024","02/21/2025"
+"Anschutz Exploration Corporation","4304757564","DRUMLIN FED 10-24-30-19-3 MCH","DRILL","10/01/2024","10/01/2025"
+"Anschutz Exploration Corporation","4304757566","DRUMLIN FED 10-24-30-5-13 MCH","DRILL","06/25/2024","06/25/2025"
+"Anschutz Exploration Corporation","4304757567","DRUMLIN FED 10-24-30-5-14 MCH","DRILL","10/01/2024","10/01/2025"
+```
