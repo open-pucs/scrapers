3993c03bcd56fc1f879cd2d62a5edbe41ddfba5e
Nicole Venner <nvenner@protonmail.ch>
2025-08-13 07:40:26 -0600

    finally fix the obvious errors relating to the code migration

--- openpuc_scrapers/scrapers/scraper_lookup.py
+++ openpuc_scrapers/scrapers/scraper_lookup.py
@@ -3,10 +3,7 @@ from pydantic import BaseModel
 
 from openpuc_scrapers.scrapers.base import GenericScraper
 from openpuc_scrapers.scrapers.dummy import DummyScraper
-from openpuc_scrapers.scrapers.il_puc import IllinoisICCScraper
-from openpuc_scrapers.scrapers.ma_puc import MassachusettsDPUScraper
 from openpuc_scrapers.scrapers.ny_puc import NYPUCDocket, NYPUCScraper
-from openpuc_scrapers.scrapers.ut_dogm import UTDOGMDocket, UTDOGMScraper
 
 
 class ScraperInfoObject(BaseModel):
@@ -22,6 +19,7 @@ SCRAPER_REGISTRY: List[ScraperInfoObject] = [
         name="New York PUC",
         object_type=NYPUCScraper,
         test_singular_docket=NYPUCDocket(
+            case_url="",
             case_number="18-G-0736",
             matter_type="Complaint",
             matter_subtype="Formal Non-Consumer Related",
@@ -31,24 +29,24 @@ SCRAPER_REGISTRY: List[ScraperInfoObject] = [
             industry_affected="Gas",  # This field wasn't provided in the comments
         ),
     ),
-    ScraperInfoObject(
-        id="ut_dogm",
-        name="Utah Divison of Oil and Gas",
-        object_type=UTDOGMScraper,
-        test_singular_docket=UTDOGMDocket(
-            case_number="18-G-0736",
-            matter_type="Complaint",
-            matter_subtype="Formal Non-Consumer Related",
-            case_title="Complaint and Formal Dispute Resolution Request For Expedited Resolution of East Coast Power & Gas, LLC Regarding Annual Reconciliation Charges of KeySpan Gas East Corporation d/b/a National Grid for January - April 2018",
-            organization="East Coast Power & Gas, LLC",
-            date_filed="12/05/2018",
-            industry_affected="Gas",  # This field wasn't provided in the comments
-        ),
-    ),
+    # ScraperInfoObject(
+    #     id="ut_dogm",
+    #     name="Utah Divison of Oil and Gas",
+    #     object_type=UTDOGMScraper,
+    #     test_singular_docket=UTDOGMDocket(
+    #         case_number="18-G-0736",
+    #         matter_type="Complaint",
+    #         matter_subtype="Formal Non-Consumer Related",
+    #         case_title="Complaint and Formal Dispute Resolution Request For Expedited Resolution of East Coast Power & Gas, LLC Regarding Annual Reconciliation Charges of KeySpan Gas East Corporation d/b/a National Grid for January - April 2018",
+    #         organization="East Coast Power & Gas, LLC",
+    #         date_filed="12/05/2018",
+    #         industry_affected="Gas",  # This field wasn't provided in the comments
+    #     ),
+    # ),
     ScraperInfoObject(id="dummy", name="Dummy Scraper", object_type=DummyScraper),
-    ScraperInfoObject(id="il_puc", name="Illinois ICC", object_type=IllinoisICCScraper),
-    ScraperInfoObject(
-        id="ma_puc", name="Massachusetts PUC", object_type=MassachusettsDPUScraper
-    ),
+    # ScraperInfoObject(id="il_puc", name="Illinois ICC", object_type=IllinoisICCScraper),
+    # ScraperInfoObject(
+    #     id="ma_puc", name="Massachusetts PUC", object_type=MassachusettsDPUScraper
+    # ),
     # ScraperInfoObject(id="co_puc", name="Colorado PUC", object_type=COPUCScraper),
 ]
