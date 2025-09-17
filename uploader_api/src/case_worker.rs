ea76dc38bb1a64c5fecaee9f44b6f3928ad9c698
Nicole Venner <nvenner@protonmail.ch>
2025-09-17 11:46:27 -0600

    fucking finally

--- uploader_api/src/case_worker.rs
+++ uploader_api/src/case_worker.rs
@@ -1,28 +1,31 @@
 use crate::s3_stuff::make_s3_client;
 use crate::types::raw::RawDocketWithJurisdiction;
+use anyhow::anyhow;
 use async_trait::async_trait;
 use aws_sdk_s3::Client;
 use chrono::Utc;
-use mycorrhiza_common::s3_generic::cannonical_location::{download_openscrapers_object, upload_object};
+use mycorrhiza_common::s3_generic::cannonical_location::{
+    download_openscrapers_object, upload_object,
+};
 use mycorrhiza_common::tasks::ExecuteUserTask;
 use openscraper_types::jurisdictions::JurisdictionInfo;
 use openscraper_types::raw::RawGenericDocket;
 use openscraper_types::s3_stuff::DocketAddress;
+use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Deserialize, Serialize)]
+#[derive(Clone, Deserialize, Serialize, JsonSchema)]
 pub struct RawDocketIngestInfo {
-    pub docket : RawGenericDocket,
+    pub docket: RawGenericDocket,
     pub jurisdiction: JurisdictionInfo,
     #[serde(default)]
     pub upload_type: RawDocketUploadType,
 }
 
-
 #[repr(transparent)]
 pub struct ProcessCaseWithoutDownload(pub RawDocketIngestInfo);
 
-#[derive(Clone,Default, Copy, Deserialize, Serialize)]
+#[derive(Clone, Default, Copy, Deserialize, Serialize, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum RawDocketUploadType {
     #[default]
@@ -39,7 +42,9 @@ impl RawDocketUploadType {
         mut original: RawGenericDocket,
     ) -> anyhow::Result<RawGenericDocket> {
         if uploaded.case_govid != original.case_govid {
-            return anyhow!("Docket id's do not match for dockets that are being merged.");
+            return Err(anyhow!(
+                "Docket id's do not match for dockets that are being merged."
+            ));
         }
         let mut modified_case = match self {
             RawDocketUploadType::All => uploaded,
@@ -58,15 +63,16 @@ impl RawDocketUploadType {
             }
         };
         modified_case.indexed_at = Utc::now();
-        Some(modified_case)
+        Ok(modified_case)
     }
     async fn download_and_compare(
         &self,
         s3_client: &Client,
         uploaded_docket: RawGenericDocket,
-        docket_addres: &DocketAddress,
+        docket_address: &DocketAddress,
     ) -> anyhow::Result<RawGenericDocket> {
-        let original_docket = download_openscrapers_object<RawGenericDocket>(s3_client, docket_addres).await?;
+        let original_docket =
+            download_openscrapers_object::<RawGenericDocket>(s3_client, docket_address).await?;
         let final_docket = self.join_dockets(uploaded_docket, original_docket)?;
         Ok(final_docket)
     }
@@ -74,19 +80,7 @@ impl RawDocketUploadType {
 #[async_trait]
 impl ExecuteUserTask for ProcessCaseWithoutDownload {
     async fn execute_task(self: Box<Self>) -> Result<serde_json::Value, serde_json::Value> {
-        let s3_client = make_s3_client().await;
-        let RawDocketIngestInfo {
-            docket,
-            jurisdiction,
-            upload_type
-        } = self.0;
-        let docket_address = DocketAddress {
-            docket_govid: docket.case_govid.clone().to_string(),
-            jurisdiction,
-        };
-        let joined_docket = upload_type.download_and_compare(&s3_client, docket, docket_addres).await.map_err(|e| e.to_string().into())?;
-        let res = upload_object::<RawGenericDocket>(&s3_client, &docket_address, &joined_docket).await;
-
+        let res = upload_docket_with_info(self.0).await;
         match res {
             Ok(()) => Ok("Task Completed Successfully".into()),
             Err(err) => Err(err.to_string().into()),
@@ -101,4 +95,27 @@ impl ExecuteUserTask for ProcessCaseWithoutDownload {
     {
         "ingest_case_with_jurisdiction_and_download"
     }
+}
+
+pub async fn upload_docket_with_info(ingest_info: RawDocketIngestInfo) -> anyhow::Result<()> {
+    let s3_client = make_s3_client().await;
+    let RawDocketIngestInfo {
+        docket,
+        jurisdiction,
+        upload_type,
+    } = ingest_info;
+    let docket_address = DocketAddress {
+        docket_govid: docket.case_govid.clone().to_string(),
+        jurisdiction,
+    };
+    let joined_docket_result = upload_type
+        .download_and_compare(&s3_client, docket, &docket_address)
+        .await;
+    let joined_docket = match joined_docket_result {
+        Ok(val) => val,
+        Err(e) => return Err(e.into()),
+    };
+    upload_object::<RawGenericDocket>(&s3_client, &docket_address, &joined_docket)
+        .await
+        .map_err(|err| err.into())
 }
--- uploader_api/src/server/queue_routes.rs
+++ uploader_api/src/server/queue_routes.rs
@@ -7,7 +7,9 @@ use openscraper_types::{
 use serde::{Deserialize, Serialize};
 use tracing::info;
 
-use crate::case_worker::{ProcessCaseWithoutDownload, RawDocketIngestInfo};
+use crate::case_worker::{
+    ProcessCaseWithoutDownload, RawDocketIngestInfo, upload_docket_with_info,
+};
 
 use mycorrhiza_common::tasks::{TaskStatusDisplay, workers::add_task_to_queue};
 
@@ -15,16 +17,19 @@ pub async fn submit_cases_to_queue_without_download(
     Json(cases): Json<Vec<RawDocketIngestInfo>>,
 ) -> impl IntoApiResponse {
     let mut return_results = vec![];
-    for case in cases {
+    for case_upload_info in cases {
         let priority = 0;
-        info!(case_number = %case.docket.case_govid, %priority, "Request received to submit case to queue");
-        let res = add_task_to_queue(ProcessCaseWithoutDownload(case), priority).await;
-        return_results.push(TaskStatusDisplay::from(res));
+        let caseid = case_upload_info.docket.case_govid.clone();
+        info!(case_number = %case_upload_info.docket.case_govid, %priority, "Request received to submit case to queue");
+        let status = add_task_to_queue(ProcessCaseWithoutDownload(case), priority).await;
+        let display_status: TaskStatusDisplay = status.into();
+        return_results.push(TaskStatusDisplay::from(display_status));
+        // let val = upload_docket_with_info(case_upload_info).await;
+        // let return_string = match val {
+        //     Ok(_) => format!("Docket {caseid} was uploaded successfully"),
+        //     Err(err) => format!("Docket {caseid} encountered an error: {err}"),
+        // };
+        // return_results.push(return_string);
     }
-    (axum::http::StatusCode::OK, Json(return_results)).into_response()
-}
-
-pub fn submit_case_to_queue_docs(op: TransformOperation) -> TransformOperation {
-    op.description("Submit a case to the processing queue.")
-        .response::<200, Json<TaskStatusDisplay>>()
+    Json(return_results)
 }
